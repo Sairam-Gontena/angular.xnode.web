b28f54b4e1924e43392e7fddb7e0288538617822
Dilip <dilipchandra@salientminds.com>
2023-05-16 15:51:09 +0530

    Component import changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { ERGeneratorModule } from './er-generator/generator.module';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppSideMenuComponent } from './components/app-side-menu/app-side-menu.component';
 import { PrimeModulesModule } from './prime-modules/prime-modules.module';
-import { TemplateBuilderPublishHeaderComponent } from './components/template-builder-publish-header/template-builder-publish-header.component';
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent, AppSideMenuComponent, TemplateBuilderPublishHeaderComponent],
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppSideMenuComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- src/app/pages/template-builder/template-builder.module.ts
+++ src/app/pages/template-builder/template-builder.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { TemplateBuilderRoutingModule } from './template-builder-routing.module';
 import { TemplateBuilderComponent } from './template-builder.component';
 import { GridsterModule } from 'angular-gridster2';
-
-
+import { TemplateBuilderPublishHeaderComponent } from 'src/app/components/template-builder-publish-header/template-builder-publish-header.component';
+import { PrimeModulesModule } from '../../prime-modules/prime-modules.module';
 
 @NgModule({
   declarations: [
-    TemplateBuilderComponent
+    TemplateBuilderComponent,
+    TemplateBuilderPublishHeaderComponent
   ],
   imports: [
     CommonModule,
     TemplateBuilderRoutingModule,
-    GridsterModule
+    GridsterModule,
+    PrimeModulesModule
   ],
   exports: [
     GridsterModule
