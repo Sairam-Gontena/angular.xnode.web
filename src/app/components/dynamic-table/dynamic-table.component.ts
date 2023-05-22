c92b1316540454449398ea030336785df748d8a4
Arun Ragam <arun.ragam@salientminds.com>
2023-05-22 14:51:40 +0530

    dynamic table changes

--- src/app/components/dynamic-table/dynamic-table.component.ts
+++ src/app/components/dynamic-table/dynamic-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChangeDetectorRef } from '@angular/core';
 import dynamicTable from '../../../assets/json/dynamic-table.json'
 
 
@@ -14,20 +14,40 @@ export class DynamicTableComponent implements OnInit {
   headers: any;
   values: any;
   heading: string = "Users";
-  editable: boolean | undefined;
-
+  public editable: boolean = false;
+  public notEditable: boolean = true;
   ngOnInit(): void {
-    this.editable = false;
     this.dynamicData = dynamicTable?.dynamicTable;
     this.headers = Object.keys(this.dynamicData[0]);
     this.values = Object.values(this.dynamicData)
   }
 
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+
   editableFunc() {
-    if (this.editable == true) {
+    if (this.editable) {
       this.editable = false;
+      this.notEditable = true;
     } else {
       this.editable = true;
+      this.notEditable = false;
     }
   }
+
+  onCellInputBlur(event: any) {
+    console.log(event.target.id)
+    console.log(event.target.value)
+  }
+
+  onCellEditComplete(event: any) {
+    const editedRowData = event.data; // edited row data
+    const editedColumnField = event.field; // edited column field
+    const newValue = event.field[event.field]; // new value
+    console.log(editedRowData)
+    console.log(editedColumnField)
+    console.log(newValue)
+  }
+
 }
