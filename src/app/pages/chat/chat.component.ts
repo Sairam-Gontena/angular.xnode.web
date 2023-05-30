76aeacb5dbc8a2e464c39ebe96a966cb576b3b44
Tulasi <mtulasip@gmail.com>
2023-05-30 16:13:15 +0530

    bot data added dynamically

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import  botChat from 'src/assets/json/bot.json';
+import  userChat from 'src/assets/json/user.json';
+
 
 @Component({
   selector: 'xnode-chat',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+  botData: any;
+  userData: any;
 
+  constructor () {
+    console.log(userChat.userChat[0]);
+    this.botData = botChat.botChat;
+    this.userData = userChat.userChat;
+  }
 }
