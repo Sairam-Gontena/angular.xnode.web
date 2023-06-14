379a72c8c8c19a5bd26db97b87104ce7400c8018
jyostna.allu <jyostna.allu@salientminds.com>
2023-06-14 11:30:21 +0530

    usecase

---
+++ src/app/pages/use-cases/use-cases-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UseCasesComponent } from './use-cases.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UseCasesComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UseCasesRoutingModule { }
