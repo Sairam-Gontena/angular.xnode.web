ef450cfc1cde8c018e489bcddbdebbb5d9b976e1
Dilip <dilipchandra@salientminds.com>
2023-06-17 04:57:49 +0530

    Chnages in configure

--- src/app/pages/configure/configure.component.ts
+++ src/app/pages/configure/configure.component.ts
@@ -10,13 +10,31 @@ export class ConfigureComponent implements OnInit {
   layout: any;
   dashboard: any;
   layoutColumns: any;
+  templates: any;
+  selectedTemplate: string = 'FinBuddy';
+  highlightedIndex: string | null = null;
+
   ngOnInit(): void {
+    this.templates = [
+      { label: 'FinBuddy' }
+    ]
   }
 
   getLayout(layout: any): void {
     console.log('layout', layout);
     if (layout)
       this.dashboard = this.layoutColumns[layout];
+  }
+
+  emitIconClicked(icon: string) {
+    if (this.highlightedIndex === icon) {
+      this.highlightedIndex = null;
+    } else {
+      this.highlightedIndex = icon;
+    }
+  }
+  openNewTab(): void {
+    window.open('http://localhost:50718/', '_blank');
   }
 
 }
--- src/app/pages/configure/configure.module.ts
+++ src/app/pages/configure/configure.module.ts
@@ -1,32 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigureRoutingModule } from './configure-routing.module';
-
-
 import { ConfigureLayoutComponent } from 'src/app/components/configure-layout/configure-layout.component';
 import { ConfigureComponent } from './configure.component';
-import { GridsterModule } from 'angular-gridster2';
 import { PrimeModulesModule } from 'src/app/prime-modules/prime-modules.module';
-
-
-
-
 @NgModule({
   declarations: [
     ConfigureLayoutComponent,
     ConfigureComponent,
-
   ],
   imports: [
     CommonModule,
     ConfigureRoutingModule,
-    GridsterModule,
     PrimeModulesModule
   ],
-
-  exports: [
-
-  ],
-
 })
+
 export class ConfigureModule { }
