5da0dd8298e0e9257958a31cb24f2186004abb72
Arun Ragam <arun.ragam@salientminds.com>
2023-06-20 10:46:46 +0530

    design dynamic changes done

--- src/app/components/page-tools-layout/page-tools-layout.component.ts
+++ src/app/components/page-tools-layout/page-tools-layout.component.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { GridsterItem } from 'angular-gridster2';
+import { Router } from '@angular/router';
 import { LayoutElementsComponent } from '../layout-elements/layout-elements.component';
 import { LAYOUT_COLUMNS } from 'src/app/constants/LayoutColumns';
+import * as sidemenu from '../../../assets/json/sidemenu-tools.json';
+
 @Component({
   selector: 'xnode-page-tools-layout',
   templateUrl: './page-tools-layout.component.html',
   styleUrls: ['./page-tools-layout.component.scss']
 })
 export class PageToolsLayoutComponent {
-
+  sideMenu = sidemenu?.sidemenu;
   dashboard: Array<GridsterItem> | undefined;
   layoutColumns: any;
+  href: any;
+  sideMenuItem: any = [];
+  selectedContainer: string = 'CONTAINER';
+
+  constructor(private router: Router) {
+    this.href = this.router.url;
+    let label;
+    if (this.href.includes('design')) {
+      label = "Design";
+    } if (this.href.includes('data-model')) {
+      label = "Config";
+    } if (this.href.includes('publish')) {
+      label = "publish";
+    }
+    this.loadSubSideMenu(label);
+  }
+
   ngOnInit() {
     this.layoutColumns = LAYOUT_COLUMNS;
     this.dashboard = LAYOUT_COLUMNS.CONTAINER;
   }
+
+  async loadSubSideMenu(sidemenuLabel: any) {
+    await this.sideMenu.filter((item: any) => {
+      if (item?.Label == sidemenuLabel) {
+        this.sideMenuItem.push(item)
+        // return item;
+      }
+    });
+    console.log(this.sideMenuItem)
+  }
+
   getLayout(layout: string): void {
     console.log('layout', layout);
     if (layout)
