19ddc8e04e34271d2bf244d9b6708b09beb92538
ashish <ashish.ranjan@salientminds.com>
2023-06-28 17:30:36 +0530

    api connection updated

--- src/app/pages/use-cases/use-cases.component.ts
+++ src/app/pages/use-cases/use-cases.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service';
 
 @Component({
   selector: 'xnode-use-cases',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./use-cases.component.scss']
 })
 export class UseCasesComponent {
+
+  data : String = '';
+  id : String = '';
+  email = 'admin@xnode.ai';
+
+  constructor( private apiService : ApiService) {}
+
+  //get calls 
+  get_ID(){
+    this.apiService.getID(this.email)
+    .then(response=> {
+      this.id = response.data;
+      console.log("In UseCaseComponent--> ID",this.id);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  }
+
+  get_Usecases(){
+    this.apiService.getUsecase(this.email, this.id)
+    .then(response => {
+      this.data = response.data;
+      console.log("In UseCaseComponent--> Insights data",this.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  }
 
 }
