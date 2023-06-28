57eb7f0a29fa662a16af8211cca4d7ed8f149095
Dilip <dilipchandra@salientminds.com>
2023-06-28 16:47:18 +0530

    Loading overview on click continue on x-pilot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'xnode-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'xnode';
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
   isUserExists() {
     return localStorage.getItem('currentUser') === 'true';
   }
 
-  parentdata:any[] =[
-      {
-        Name:"Thimma chowdary",
-        Age:25,
-        Address:"Address1",
-        Email:'thimma@gmail.comm'
-      },
-      {
-        Name:"Thimma1",
-        Age:26,
-        Address:"Address12",
-        Email:'thimma@gmail.comm' 
-  
-      },
-      {
-        Name:"Thimma1",
-        Age:26,
-        Address:"Address12",
-        Email:'thimma@gmail.comm'
-  
-      },
-    ]
+  parentdata: any[] = [
+    {
+      Name: "Thimma chowdary",
+      Age: 25,
+      Address: "Address1",
+      Email: 'thimma@gmail.comm'
+    },
+    {
+      Name: "Thimma1",
+      Age: 26,
+      Address: "Address12",
+      Email: 'thimma@gmail.comm'
+
+    },
+    {
+      Name: "Thimma1",
+      Age: 26,
+      Address: "Address12",
+      Email: 'thimma@gmail.comm'
+
+    },
+  ]
 
 }
