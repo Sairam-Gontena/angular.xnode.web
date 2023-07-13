722ac8edc168fe5ffc292d55db86aa2874075f24
Dilip <dilipchandra@salientminds.com>
2023-07-13 16:18:37 +0530

    config related changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://prod-xnode.azurewebsites.net/',
+    xpilotUrl: 'https://prod-xpilot.azurewebsites.net/',
+    designStudioUrl: ' https://prod-xnode-tmpbld.azurewebsites.net/',
+}
