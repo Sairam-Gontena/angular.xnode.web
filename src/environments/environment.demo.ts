275c5965793da41346062fb2e33a8811ca513e13
Dilip <dilipchandra@salientminds.com>
2023-07-13 12:18:47 +0530

    env setup

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://demo-xnode.azurewebsites.net/',
+    xpilotUrl: 'https://demo-xpilot.azurewebsites.net/',
+    designStudioUrl: ' https://demo-xnode-tmpbld.azurewebsites.net/',
+}
