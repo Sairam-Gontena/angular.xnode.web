46198b0395c8918952ab07458b7f49028fb43c0a
Dilip <dilipchandra@salientminds.com>
2023-07-13 18:04:51 +0530

    env related changes

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'https://demo-xnode.azurewebsites.net/',
-    xpilotUrl: 'https://demo-xpilot.azurewebsites.net/',
+    xpilotUrl: 'https://xpilot.azurewebsites.net/',
     designStudioUrl: ' https://demo-xnode-tmpbld.azurewebsites.net/',
 }
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'https://qa-xnode.azurewebsites.net/',
-    xpilotUrl: 'https://qa-xpilot.azurewebsites.net/',
+    xpilotUrl: 'https://xpilot.azurewebsites.net/',
     designStudioUrl: ' https://qa-xnode-tmpbld.azurewebsites.net/',
 }
