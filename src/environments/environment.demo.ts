1fcf0d739f6d1f581030c1390c55a5364f512ea0
Dilip <dilipchandra@salientminds.com>
2023-07-17 16:05:53 +0530

    Added publish end point in env

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -3,4 +3,5 @@ export const environment = {
     baseUrl: 'https://demo-xnode.azurewebsites.net/',
     xpilotUrl: 'https://demo-navi.azurewebsites.net/',
     designStudioUrl: ' https://demo-xnode-tmpbld.azurewebsites.net/',
+    publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/' //for now we have only dev url
 }
