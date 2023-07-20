87133d0c7e5f148956681dc5f1c38692783ad725
Thimma chowdary <thimma.chowdary@salientminds.com>
2023-07-20 18:10:07 +0530

    notification ui with pusher integration done

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderItems } from '../../constants/AppHeaderItems'
 import { Router } from '@angular/router';
+import Pusher from 'pusher-js';
+
+
 
 @Component({
   selector: 'xnode-app-header',
   templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss']
+  styleUrls: ['./app-header.component.scss'],
 })
 
 export class AppHeaderComponent implements OnInit {
   headerItems: any;
   logoutDropdown: any;
   selectedValue: any;
+  enableNotificationCard: boolean = false;
+  pusher: Pusher;
+  channel: any;
+  email: String = '';
+  notifications: any[] = [];
+
 
   constructor(private router: Router) {
+    // Create a new Pusher instance with your Pusher App Key and options
+    this.pusher = new Pusher('f641637ec102c219e3b5', {
+      cluster: 'us2'
+    });
   }
 
   ngOnInit(): void {
@@ -27,5 +40,22 @@ export class AppHeaderComponent implements OnInit {
         }
       },
     ];
+    this.pusherInitializer()
   }
+
+  pusherInitializer() {
+    let currentUser = localStorage.getItem('currentUser');
+    if (currentUser) {
+      this.email = JSON.parse(currentUser).email;
+    }
+    this.channel = this.pusher.subscribe('navi-notifier');
+    this.channel.bind(this.email, (data: any) => {
+      this.notifications.push(data);
+    });
+  }
+
+  toggleAccordion() {
+    this.enableNotificationCard = !this.enableNotificationCard
+  }
+
 }
