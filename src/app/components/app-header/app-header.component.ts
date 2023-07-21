67560b92c601e22719a0f3bee761b1bf2b851df4
Dilip <dilipchandra@salientminds.com>
2023-07-21 19:53:16 +0530

    changes regarding notif

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderItems } from '../../constants/AppHeaderItems'
 import { Router } from '@angular/router';
 import Pusher from 'pusher-js';
-
-
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'xnode-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss'],
+  providers: [MessageService]
 })
 
 export class AppHeaderComponent implements OnInit {
@@ -23,7 +23,7 @@ export class AppHeaderComponent implements OnInit {
   notificationCount: Number = 0;
 
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private messageService: MessageService) {
     // Create a new Pusher instance with your Pusher App Key and options
     this.pusher = new Pusher('f641637ec102c219e3b5', {
       cluster: 'us2'
@@ -31,17 +31,22 @@ export class AppHeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.messageService.add({ severity: 'warning', summary: '', detail: "test" });
     this.headerItems = HeaderItems;
     this.logoutDropdown = [
       {
         label: 'Logout',
         command: () => {
           localStorage.clear();
-          this.router.navigate(['/'])
+          this.router.navigate(['/']);
         }
       },
     ];
     this.pusherInitializer()
+  }
+
+  goToProducts(): void {
+    this.router.navigate(['/my-templates']);
   }
 
   pusherInitializer() {
@@ -51,6 +56,9 @@ export class AppHeaderComponent implements OnInit {
     }
     this.channel = this.pusher.subscribe('navi-notifier');
     this.channel.bind(this.email, (data: any) => {
+      console.log('data>>>', data);
+      this.messageService.clear();
+      // this.messageService.add({ severity: 'warning', summary: '', detail: data.message });
       this.notifications.push(data);
       this.notificationCount = this.notifications.length
     });
