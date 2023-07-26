48e6e1f69911ae8b0c6a31ab41b94e19d0d0e870
Dilip <dilipchandra@salientminds.com>
2023-07-26 22:04:36 +0530

    added channel in env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     baseUrl: 'https://dev-xnode.azurewebsites.net/',
     xpilotUrl: 'https://develop-navi.azurewebsites.net/',
-    designStudioUrl: 'https://dev-ui-gen.azurewebsites.net/',
-    publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/'
+    designStudioUrl: 'https://dev-ui-gen.azurewebsites.net/dashboard/',
+    publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/',
+    webSocketNotifier: 'dev-xnode-notifier'
 }
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: false,
     baseUrl: 'https://qa-xnode.azurewebsites.net/',
     xpilotUrl: 'https://qa-navi.azurewebsites.net/',
-    designStudioUrl: 'https://qa-ui-gen.azurewebsites.net/',
-    publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/'  //for now we have only dev url
+    designStudioUrl: 'https://qa-ui-gen.azurewebsites.net/dashboard/',
+    publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/',  //for now we have only dev url
+    webSocketNotifier: 'qa-xnode-notifier'
+
 }
