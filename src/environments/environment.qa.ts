8d6353a59a4ae32e9df03d0f22cbc5073e6530f3
Dilip <dilipchandra@salientminds.com>
2023-07-26 16:02:06 +0530

    env setup of design studio

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     baseUrl: 'https://qa-xnode.azurewebsites.net/',
     xpilotUrl: 'https://qa-navi.azurewebsites.net/',
-    designStudioUrl: 'https://qa-xnode-tmpbld.azurewebsites.net/',
+    designStudioUrl: 'https://qa-ui-gen.azurewebsites.net/',
     publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/'  //for now we have only dev url
 }
