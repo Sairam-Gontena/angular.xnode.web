e1818f7a25b54e3dac1799518db67ca582510f5c
Thimma chowdary <thimma.chowdary@salientminds.com>
2023-07-26 10:38:32 +0530

    simplified notification functionality

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -19,20 +19,22 @@ export class AppHeaderComponent implements OnInit {
   channel: any;
   email: string = '';
   notifications: any[] = []
+  activeFilter: string = '';
   filterTypes: any = {
     recent: false,
     important: false,
-    pined: false,
+    pinned: false,
     all: true
   }
   allNotifications: any[] =
     [
-      { id: 1, title: 'this is your 1 message', description: 'this is a description', status: 'success', type: 'notification', receivedon: '1s', read: false, important: false, pined: false, recent: true },
-      { id: 2, title: 'this is your 2 message', description: 'this is a description', status: 'pending', type: 'notification', receivedon: '5m', read: false, important: false, pined: true, recent: true },
-      { id: 3, title: 'this is your 3 message', description: 'this is a description', status: 'reject', type: 'notification', receivedon: '2m', read: true, important: true, pined: true, recent: false },
-      { id: 4, title: 'this is your 4 message', description: 'this is a description', type: 'request', img: '', receivedon: '1h', read: true, important: false, pined: false, recent: true },
-      { id: 5, title: 'this is your 5 message', description: 'this is a description', type: 'publish', icon: 'bell', receivedon: '10s', read: false, important: true, pined: true, recent: false },];
+      { id: 1, title: 'Your finbuddy app got published', description: 'Your finbuddy app got published and ready to use with the url https://finbuddy.com', status: 'success', type: 'notification', receivedon: '1s', read: false, important: false, pinned: false, recent: true },
+      { id: 2, title: 'Your todo app is in pending', description: 'Your todo app is in pending state publish your todo app', status: 'pending', type: 'notification', receivedon: '5m', read: false, important: false, pinned: true, recent: true },
+      { id: 3, title: 'Finbuddy app declined', description: 'Your todo app got rejected for in security rules, please update the security rules and publish the app', status: 'reject', type: 'notification', receivedon: '2m', read: true, important: true, pinned: true, recent: false },
+      { id: 4, title: 'Tim requested finbuddy app to publish', type: 'request', img: '', receivedon: '1h', read: true, important: false, pinned: false, recent: true },
+      { id: 5, title: 'Your app got published', description: 'Your app got published and ready to use with url https://example.com', type: 'publish', icon: 'bell', receivedon: '10s', read: false, important: true, pinned: true, recent: false },];
   notificationCount: any = 0;
+
 
   constructor(private router: Router, private messageService: MessageService, private webSocketService: WebSocketService) {
   }
@@ -79,68 +81,37 @@ export class AppHeaderComponent implements OnInit {
     this.router.navigate(['/activity'])
   }
 
-
   filterNotifications(val: any) {
-    if (val == 'recent') {
-      this.filterTypes = {
-        recent: true,
-        important: false,
-        pined: false,
-        all: false
-      }
-    } else if (val == 'important') {
-      this.filterTypes = {
-        recent: false,
-        important: true,
-        pined: false,
-        all: false
-      }
-    }
-    else if (val == 'pined') {
+    if (val === 'all') {
       this.filterTypes = {
         recent: false,
         important: false,
-        pined: true,
-        all: false
-      }
-    }
-    else if (val == 'all') {
-      this.filterTypes = {
-        recent: false,
-        important: false,
-        pined: false,
+        pinned: false,
         all: true
-      }
-    }
-    if (val == 'all') {
-      this.notifications = this.allNotifications
+      };
+      this.notifications = this.allNotifications;
     } else {
-      this.notifications = this.allNotifications.filter(x => x[val])
-
+      this.filterTypes = {
+        recent: false,
+        important: false,
+        pinned: false,
+        all: false,
+        [val]: true
+      };
+      this.notifications = this.allNotifications.filter((x) => x[val]);
     }
+
   }
 
   toggleNotificationRead(val: any, id: number) {
-    const index = this.allNotifications.findIndex(item => item.id == id);
-    if (val === 'read') {
-      this.allNotifications[index].read = true;
-    } else if (val === 'unread') {
-      this.allNotifications[index].read = false;
-    }
-    this.notifications = this.allNotifications
-    this.notifications = this.allNotifications.filter(x => x['read'])
-
+    const index = this.allNotifications.findIndex(item => item.id === id);
+    this.allNotifications[index].read = val === 'read';
   }
+
+
   toggleNotificationPinned(val: any, id: number) {
-    const index = this.allNotifications.findIndex(item => item.id == id);
-
-    if (val === 'pined') {
-      this.allNotifications[index].pined = true;
-    } else if (val === 'unpined') {
-      this.allNotifications[index].pined = false;
-    }
-    this.notifications = this.allNotifications.filter(x => x['pined'])
-
+    const index = this.allNotifications.findIndex(item => item.id === id);
+    this.allNotifications[index].pinned = val === 'pinned';
   }
 
 
