752aa2af0e5685c2d00303fc5570d5474d07e52b
Thimma chowdary <thimma.chowdary@salientminds.com>
2023-07-31 19:09:23 +0530

    dropdown donw

--- src/app/components/template-builder-publish-header/template-builder-publish-header.component.ts
+++ src/app/components/template-builder-publish-header/template-builder-publish-header.component.ts
@@ -9,6 +9,10 @@ interface DropdownOption {
   label: string;
   value: string;
 }
+interface Product {
+  name: string;
+  value: string;
+}
 @Component({
   selector: 'xnode-template-builder-publish-header',
   templateUrl: './template-builder-publish-header.component.html',
@@ -20,9 +24,6 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
   @Output() loadSpinnerInParent: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Input() productId?: string;
   selectedOption = 'PREVIEW';
-  selectedProductName = {};
-  templates: any;
-  selectedTemplate = null
   selectedDeviceIndex: string | null = null;
   templatesOptions: DropdownOption[] = [];
   templateEvent: any;
@@ -31,6 +32,11 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
   options: any[] = [{ "name": "preview" }, { "name": "published" }];
   selectedDropDownOption: string = 'Preview'
   productOptions: MenuItem[];
+
+  templates: Product[] | undefined;
+  selectedTemplate: Product | undefined
+
+
   constructor(private apiService: ApiService, private router: Router, private messageService: MessageService) {
     this.currentUser = UserUtil.getCurrentUser();
     this.productOptions = [
@@ -57,7 +63,17 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
       this.showDeviceIcons = true;
     }
     this.getAllProducts()
+    let name = localStorage.getItem('app_name')
+    let value = localStorage.getItem('record_id')
+    this.selectedTemplate = { name: name ? name : '', value: value ? value : '' };
   };
+
+  refreshCurrentRoute(): void {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
+    });
+  }
 
   deviceIconClicked(icon: string) {
     if (this.selectedDeviceIndex === icon) {
@@ -87,7 +103,6 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
       }
       this.apiService.publishApp(body)
         .then(response => {
-          console.log('response', response);
           if (response) {
             this.messageService.add({ severity: 'success', summary: '', detail: 'Your app publishing process started. You will get the notifications', sticky: true });
             this.loadSpinnerInParent.emit(false);
@@ -109,7 +124,7 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
       .then(response => {
         if (response?.status === 200 && response.data.data?.length) {
           const data = response.data.data.map((obj: any) => ({
-            label: obj.title,
+            name: obj.title,
             value: obj.id
           }));
           this.templates = data;
@@ -120,9 +135,10 @@ export class TemplateBuilderPublishHeaderComponent implements OnInit {
       });
   }
   selectedProduct(data: any): void {
-    const app_name = this.templates.filter((item: any) => item.value === data.value)[0].label
-    localStorage.setItem('record_id', data.value);
-    localStorage.setItem('app_name', app_name);
-    this.router.navigate(['/design']);
+    const app_name = this.templates?.filter((item: any) => item.value === data.value.value)[0].name
+    localStorage.setItem('record_id', data.value.value);
+    localStorage.setItem('app_name', app_name ? app_name : '');
+    this.selectedTemplate = { name: app_name ? app_name : '', value: data.value.value };
+    this.refreshCurrentRoute()
   }
 }
