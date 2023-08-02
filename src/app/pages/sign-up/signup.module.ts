41f027b027c8eec61f678a8c861d369281ea06f3
jyostna.allu <jyostna.allu@salientminds.com>
2023-08-02 13:14:20 +0530

    updated the code

--- src/app/pages/sign-up/signup.module.ts
+++ src/app/pages/sign-up/signup.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
 import { SignUpRoutingModule } from './signup-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { PrimeModule } from 'src/app/prime-modules/prime.module';
+import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
+import {
+    SocialLoginModule,
+    SocialAuthServiceConfig,
+} from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+
+import { Component } from '@angular/core';
+
+
 @NgModule({
     declarations: [
         SignUpComponent,
@@ -17,7 +28,25 @@ import { PasswordModule } from 'primeng/password';
         ReactiveFormsModule,
         ButtonModule,
         PasswordModule,
-        DividerModule
-    ]
+        DividerModule,
+        GoogleSigninButtonModule,
+        SocialLoginModule,
+
+    ],
+    providers: [
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin: false,
+                providers: [
+                    {
+                        id: GoogleLoginProvider.PROVIDER_ID,
+                        provider: new GoogleLoginProvider('428503221481-aul4gv3ulnehnf46jovp602rc5j2hckv.apps.googleusercontent.com'),
+                    },
+
+                ],
+            } as SocialAuthServiceConfig,
+        },
+    ],
 })
 export class SignUpModule { }
