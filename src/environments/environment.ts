f4c15911906c57d44accdca5a4aac2ab8d790a4a
Dilip <dilipchandra@salientminds.com>
2023-08-10 12:40:09 +0530

    minor change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     baseUrl: 'http://localhost:4200/',
     xpilotUrl: 'http://localhost:8000/',
-    naviUrl: 'http://localhost:4202/',
+    naviUrl: 'http://localhost:4202',
     designStudioUrl: 'http://localhost:4201/dashboard/',
     publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular',
     webSocketNotifier: 'dev-xnode-notifier',
