b7844b5aee292b926b13466f630312fde0498ff4
Dilip <dilipchandra@salientminds.com>
2023-08-10 13:33:30 +0530

    navi url added in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     baseUrl: 'https://prod-xnode.azurewebsites.net/',
     xpilotUrl: 'https://prod.azurewebsites.net/',
+    naviUrl: 'https://prod-navi-web.azurewebsites.net',
     designStudioUrl: 'https://prod-ui-gen.azurewebsites.net/',
     publishUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/',  //for now we have only dev url
     workFlowUrl: 'https://dev-xnode-xflows.azurewebsites.net/' //Prod env not configured yet
