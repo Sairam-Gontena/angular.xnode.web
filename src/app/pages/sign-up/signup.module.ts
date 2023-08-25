f88b4af8fe51b9fe9659f472eaf4bbbb719f2126
jyostna.allu <jyostna.allu@salientminds.com>
2023-08-25 15:16:19 +0530

    popup to signup page

--- src/app/pages/sign-up/signup.module.ts
+++ src/app/pages/sign-up/signup.module.ts
@@ -6,39 +6,41 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
-import {  GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
+import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 import {
-    SocialLoginModule,
-    SocialAuthServiceConfig,
-  } from '@abacritt/angularx-social-login';
-  import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+} from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
-    declarations: [
-        SignUpComponent,
-    ],
-    imports: [
-        CommonModule,
-        SignUpRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        ButtonModule,
-        PasswordModule,
-        DividerModule,
-        GoogleSigninButtonModule,
-        SocialLoginModule
-    ],
-    providers:[{
-        provide: 'SocialAuthServiceConfig',
-        useValue: {
-          autoLogin: false,
-          providers: [
-            {
-              id: GoogleLoginProvider.PROVIDER_ID,
-              provider: new GoogleLoginProvider('429647457919-gov40lg2ok2gglfro438vchcrqqa0kqe.apps.googleusercontent.com'),
-            },
-          ],
-        } as SocialAuthServiceConfig,
-      }]
+  declarations: [
+    SignUpComponent,
+  ],
+  imports: [
+    CommonModule,
+    SignUpRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    PasswordModule,
+    DividerModule,
+    GoogleSigninButtonModule,
+    SocialLoginModule,
+    DialogModule
+  ],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('429647457919-gov40lg2ok2gglfro438vchcrqqa0kqe.apps.googleusercontent.com'),
+        },
+      ],
+    } as SocialAuthServiceConfig,
+  }]
 })
 export class SignUpModule { }
