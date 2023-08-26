de5d1758f48a7c84ca4a9bc512ea005e1bf937a6
Dilip <dilipchandra@salientminds.com>
2023-08-26 10:43:45 +0530

    env related changes of auth end point

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     apiUrl: 'https://dev-navi.azurewebsites.net/',
+    authApiUrl: 'https://dev-xnode-auth-api.azurewebsites.net/',
     xnodeAppUrl: 'https://dev-xnode.azurewebsites.net/',
     naviAppUrl: 'https://dev-navi-web.azurewebsites.net/',
-    xnodeAuthUrl:'https://dev-xnode-auth-api.azurewebsites.net/',
     designStudioAppUrl: 'https://dev-ui-gen.azurewebsites.net/dashboard/',
     publishApiUrl: 'https://dev-frontend-builder.azurewebsites.net/ui/angular/',
     workFlowApiUrl: 'https://dev-xnode-xflows.azurewebsites.net/',
