1d894dcebc90115372747b0987539ed0b1a4f9c5
Dilip <dilipchandra@salientminds.com>
2023-09-04 19:24:10 +0530

    common api added in all envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     apiUrl: 'https://prod.azurewebsites.net/',
     authApiUrl: 'https://prod-xnode-auth-api.azurewebsites.net/',
     userUtilsApi: 'https://prod-user-util-api.azurewebsites.net/',
+    commonApiUrl: 'https://prod-common-api.azurewebsites.net/',
     xnodeAppUrl: 'https://prod-xnode.azurewebsites.net/',
     naviAppUrl: 'https://prod-navi-web.azurewebsites.net/',
     designStudioAppUrl: 'https://prod-ui-gen.azurewebsites.net/',
