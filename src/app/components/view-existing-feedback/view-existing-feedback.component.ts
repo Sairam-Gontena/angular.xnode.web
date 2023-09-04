42ad141d854774d19a854d29ca7146c38b4bbd71
arun.ragam <arun.ragam@salientminds.com>
2023-09-04 16:30:14 +0530

    help center, feedback changes

--- src/app/components/view-existing-feedback/view-existing-feedback.component.ts
+++ src/app/components/view-existing-feedback/view-existing-feedback.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'xnode-view-existing-feedback',
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-existing-feedback.component.scss']
 })
 export class ViewExistingFeedbackComponent {
+
+  public getScreenWidth: any;
+  public dialogWidth: string = '80vw';
   visible = true;
   areaTypes: any[] = [{ label: 'Reported Bug', value: 'RP' }, { label: 'General Feedback', value: 'GF' }];
   selectedArea: any;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    if (this.getScreenWidth < 780) {
+      this.dialogWidth = '100vw';
+    } else if (this.getScreenWidth > 780 && this.getScreenWidth < 980) {
+      this.dialogWidth = '80vw';
+    } else if (this.getScreenWidth > 980) {
+      this.dialogWidth = '80vw';
+    }
+  }
+
+  constructor() {
+    this.onWindowResize();
+  }
 }
