7d1be64b73592a09a4d0e291a090361662542731
arun.ragam <arun.ragam@salientminds.com>
2023-09-28 12:35:18 +0530

    product alert changes

--- src/app/components/product-alert-popup/product-alert-popup.component.ts
+++ src/app/components/product-alert-popup/product-alert-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api/api.service';
 import { UtilsService } from '../services/utils.service';
 import { User, UserUtil } from 'src/app/utils/user-util';
@@ -9,25 +9,79 @@ import { AuditutilsService } from 'src/app/api/auditutils.service'
   templateUrl: './product-alert-popup.component.html',
   styleUrls: ['./product-alert-popup.component.scss']
 })
-export class ProductAlertPopupComponent {
+export class ProductAlertPopupComponent implements OnInit {
   @Input() showProductStatusPopup: any;
   @Output() closePopup = new EventEmitter<boolean>();
   @Output() openDockedNavi = new EventEmitter<Object>();
+  @Input() data: any;
 
   visible = true;
   consversationList = [];
   currentUser?: User;
+  dialogHeader: string = 'Product Status';
+  buttonLabel: string = '';
+  product_id: any;
 
   constructor(private apiService: ApiService, private utils: UtilsService, private auditUtil: AuditutilsService,) {
     this.currentUser = UserUtil.getCurrentUser();
   }
+
+  ngOnInit(): void {
+    if (this.data.content) {
+      this.dialogHeader = 'Confirm ' + this.data.content
+      switch (this.data.content) {
+        case "App Generation": {
+          this.buttonLabel = 'Generate app';
+          break;
+        }
+        case "App Publishing": {
+          this.buttonLabel = 'Publish app';
+          break;
+        }
+        case "Spec Generation": {
+          this.buttonLabel = 'Generate Spec'
+          break;
+        }
+        default: {
+          this.buttonLabel = 'Generate Application';
+          break;
+        }
+      }
+      this.product_id = this.data?.product_id;
+      this.consversationList = JSON.parse(this.data.conversation);
+    } else {
+      this.product_id = localStorage.getItem('record_id');
+    }
+  }
+
   continueChat(): void {
     this.closePopup.emit(true);
   }
 
+  actionButton() {
+    switch (this.buttonLabel) {
+      case "App Generation": {
+        this.persistConversaiton();
+        break;
+      }
+      case "App Publishing": {
+        this.buttonLabel = 'Publish app';
+        break;
+      }
+      case "Spec Generation": {
+        console.log('spec generation clicked TBD')
+        break;
+      }
+      case "Generate Application": {
+        this.getPreviousCoversation();
+        break;
+      }
+    }
+  }
+
   getPreviousCoversation(): void {
     this.utils.loadSpinner(true);
-    this.apiService.get('/get_conversation/' + this.currentUser?.email + '/' + localStorage.getItem('record_id')).then((res: any) => {
+    this.apiService.get('/get_conversation/' + this.currentUser?.email + '/' + this.product_id).then((res: any) => {
       if (res.status === 200 && res.data) {
         this.consversationList = res.data?.conversation_history;
         this.persistConversaiton();
@@ -50,7 +104,7 @@ export class ProductAlertPopupComponent {
     const persistconversation = {
       "email": this.currentUser?.email,
       "conversation_history": this.consversationList,
-      "product_id": localStorage.getItem('record_id')
+      "product_id": this.product_id
     }
     this.apiService.post(persistconversation, '/persist_conversation').then((response: any) => {
       if (response) {
