12dd79a628f4d7fc2f2ae6112def35154acbbaaf
Dilip <dilipchandra@salientminds.com>
2023-09-28 20:03:28 +0530

    enabled spec for Beta

--- src/app/constants/AppSideMenuItems.ts
+++ src/app/constants/AppSideMenuItems.ts
@@ -191,6 +191,11 @@ export const AppSideMenuItems =
                 path: 'overview'
             },
             {
+                label: 'Spec',
+                icon: './assets/overview.svg',
+                path: 'specifications'
+            },
+            {
                 label: 'Usecase',
                 icon: './assets/overview.svg',
                 path: 'usecases'
