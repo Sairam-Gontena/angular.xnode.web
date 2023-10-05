961a2e00839d6936f9e06a4cf1d4398687615a65
Apple <apple@Apples-MacBook-Pro.local>
2023-10-05 15:07:45 +0530

    expand feature for dashboard in spec

--- src/app/components/expand-specification/expand-specification.component.ts
+++ src/app/components/expand-specification/expand-specification.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 declare const SwaggerUIBundle: any;
 
@@ -12,9 +13,23 @@ export class ExpandSpecificationComponent {
   @Input() dataToExpand: any;
   @Output() dataFlowEmitter = new EventEmitter<any>();
 
+  iframeSrc: SafeResourceUrl = '';
+  targetUrl: string = environment.naviAppUrl;
+
+  constructor(private domSanitizer: DomSanitizer,) {
+
+  }
+
   ngOnInit() {
-    console.log("inside expand component");
-    console.log(this.dataToExpand)
+    const record_id = localStorage.getItem('record_id');
+    let userData: any
+    userData = localStorage.getItem('currentUser');
+    let email = JSON.parse(userData).email;
+    let user_id = JSON.parse(userData).id;
+    if (record_id) {
+      this.targetUrl = environment.designStudioAppUrl + "?email=" + email + "&id=" + record_id + "&targetUrl=" + environment.xnodeAppUrl + "&has_insights=" + true + '&isVerified=true' + "&userId=" + user_id;
+    }
+    this.makeTrustedUrl();
   }
 
   ngAfterViewInit() {
@@ -42,5 +57,9 @@ export class ExpandSpecificationComponent {
       docExpansion: 'none',
       operationsSorter: 'alpha'
     });
+  }
+
+  makeTrustedUrl(): void {
+    this.iframeSrc = this.domSanitizer.bypassSecurityTrustResourceUrl(this.targetUrl);
   }
 }
