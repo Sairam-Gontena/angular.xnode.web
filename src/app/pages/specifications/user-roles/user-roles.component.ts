3ee122dd25980125bcd1d17af17f10e188c53574
Dilip <dilipchandra@salientminds.com>
2023-10-20 16:21:18 +0530

    user roles related changes

---
+++ src/app/pages/specifications/user-roles/user-roles.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'xnode-user-roles',
+  templateUrl: './user-roles.component.html',
+  styleUrls: ['./user-roles.component.scss']
+})
+export class UserRolesComponent {
+  @Input() content: any;
+}
