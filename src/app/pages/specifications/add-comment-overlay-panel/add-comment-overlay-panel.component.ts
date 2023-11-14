a4b19fd8223f7b429d592b672959ad9695120296
Apple <apple@Apples-MacBook-Pro.local>
2023-11-14 15:05:48 +0530

    comments edited successfully #8575

--- src/app/pages/specifications/add-comment-overlay-panel/add-comment-overlay-panel.component.ts
+++ src/app/pages/specifications/add-comment-overlay-panel/add-comment-overlay-panel.component.ts
@@ -23,6 +23,8 @@ export class AddCommentOverlayPanelComponent implements OnInit {
   @Input() parentId: any;
   @Input() topParentId: any;
   @Input() commentType: string = '';
+  @Input() selectedComment: any;
+  @Input() action: any;
   assinedUsers: string[] = [];
   assignAsaTask: boolean = false;
   currentUser: any;
@@ -65,19 +67,25 @@ export class AddCommentOverlayPanelComponent implements OnInit {
   }
 
   onClickSend(): void {
-    let body = {
-      "createdBy": this.currentUser.user_id,
-      "topParentId": this.topParentId, // For new comment it is 'null' and reply level this should be top comment id.
-      "parentEntity": this.parentEntity,
-      "parentId": this.parentId, // It should be spec id at New comment level and parent commment id at reply level
-      "message": this.comment,
-      "referenceContent": this.parentEntity === 'SPEC' ? { title: this.selectedContent.title, content: this.selectedContent.content } : {},
-      "attachments": [
-      ],
-      "references": { Users: this.references },
-      "followers": [
-      ],
-      "feedback": {}
+    let body;
+    if (this.action === 'EDIT') {
+      this.selectedComment.message = this.comment;
+      body = this.selectedComment;
+    } else {
+      body = {
+        "createdBy": this.currentUser.user_id,
+        "topParentId": this.topParentId, // For new comment it is 'null' and reply level this should be top comment id.
+        "parentEntity": this.parentEntity,
+        "parentId": this.parentId, // It should be spec id at New comment level and parent commment id at reply level
+        "message": this.comment,
+        "referenceContent": this.parentEntity === 'SPEC' ? { title: this.selectedContent.title, content: this.selectedContent.content } : {},
+        "attachments": [
+        ],
+        "references": { Users: this.references },
+        "followers": [
+        ],
+        "feedback": {}
+      }
     }
     this.saveComment(body);
   }
@@ -89,7 +97,11 @@ export class AddCommentOverlayPanelComponent implements OnInit {
         this.utils.openOrClosePanel(SidePanel.Comments);
         this.comment = '';
         this.closeOverlay.emit();
-        this.utils.loadToaster({ severity: 'success', summary: 'SUCCESS', detail: 'Comment added successfully' });
+        let detail = 'Comment added successfully'
+        if (this.action == 'EDIT') {
+          detail = 'Comment edited successfully'
+        }
+        this.utils.loadToaster({ severity: 'success', summary: 'SUCCESS', detail });
       } else {
         this.utils.loadToaster({ severity: 'error', summary: 'ERROR', detail: commentsReponse?.data?.common?.status });
       }
