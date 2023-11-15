43a7bbe72f360835dad52d2f74523883d6c2aeff
Tulasi <tulasi.polisetti@salientminds.com>
2023-11-15 14:29:40 +0530

    #8607 timestamp added in comments and tasks in spec

--- src/app/pages/specifications/spec-conversation/spec-conversation.component.ts
+++ src/app/pages/specifications/spec-conversation/spec-conversation.component.ts
@@ -116,6 +116,26 @@ export class SpecConversationComponent {
     });
     return this.sanitizer.bypassSecurityTrustHtml(highlighted);
   }
+  modifiedTimeDifference(modifiedOn: Date): string {
+    const now = new Date();
+    const modifiedTime = new Date(modifiedOn);
+    const timeDifference = now.getTime() - modifiedTime.getTime();
+    const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+    if (minutesDifference < 1) {
+      return 'Just now';
+    } else if (minutesDifference === 1) {
+      return '1 m ago';
+    } else if (minutesDifference < 60) {
+      return `${minutesDifference} m ago`;
+    } else {
+      const hoursDifference = Math.floor(minutesDifference / 60);
+      if (hoursDifference === 1) {
+        return '1 h ago';
+      } else {
+        return `${hoursDifference} h ago`;
+      }
+    }
+  }
 
   viewReplies(cmt?: any) {
     if (!cmt.topParentId || cmt.topParentId !== null) {
