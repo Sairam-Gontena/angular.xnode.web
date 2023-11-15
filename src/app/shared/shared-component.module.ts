e895a285b09e16aaf804491694598c5c62058910
arun.ragam <arun.ragam@salientminds.com>
2023-11-15 16:24:01 +0530

    data missing in spec threads and user roles comment overlay added

--- src/app/shared/shared-component.module.ts
+++ src/app/shared/shared-component.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdditionalInfoComponent } from '../components/additional-info/additional-info.component';
 import { AppHeaderComponent } from '../components/app-header/app-header.component';
@@ -123,6 +123,7 @@ import { ProductDropdownComponent } from '../components/product-dropdown/product
                 CommonSpecTableComponent,
                 ProductDropdownComponent,
                 SharedPipesModule
-        ]
+        ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedComponentModule { }
