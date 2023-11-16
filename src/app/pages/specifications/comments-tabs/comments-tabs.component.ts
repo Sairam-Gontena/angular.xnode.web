825b45fdcb16c1085e95eb9ab7c1e6659a96bca0
Apple <apple@Apples-MacBook-Pro.local>
2023-11-16 14:08:52 +0530

    code refactor to simplify the code in comment and task list #8636

--- src/app/pages/specifications/comments-tabs/comments-tabs.component.ts
+++ src/app/pages/specifications/comments-tabs/comments-tabs.component.ts
@@ -8,11 +8,10 @@ import { UtilsService } from 'src/app/components/services/utils.service';
   styleUrls: ['./comments-tabs.component.scss']
 })
 export class CommentsTabsComponent implements OnInit {
-  @Input() commentList: Array<Comment> = [];
-  @Input() tasksList: Array<Comment> = [];
+  @Input() list: Array<Comment> = [];
   @Input() usersList: any;
-  list: Array<Comment> = [];
   activeIndex: number = 0;
+  tabTypes: Array<string> = ['Conversation', 'Tasks'];
 
   constructor(public utils: UtilsService) {
     this.utils.getMeTaskAssigned.subscribe((taskAssigned) => {
@@ -26,25 +25,12 @@ export class CommentsTabsComponent implements OnInit {
 
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this.activeIndex === 0) {
-      this.list = this.commentList;
-    } else if (this.activeIndex === 1) {
-      this.list = this.tasksList;
-    }
-  }
-
-
   onTabChange(event: any) {
     this.activeIndex = event.index;
-    this.updateCommentsList();
-  }
-
-  updateCommentsList() {
     if (this.activeIndex === 0) {
-      this.list = this.commentList;
+      this.utils.updateCommnetsList('comment')
     } else if (this.activeIndex === 1) {
-      this.list = this.tasksList;
+      this.utils.updateCommnetsList('task')
     }
   }
 }
