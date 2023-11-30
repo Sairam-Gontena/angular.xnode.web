3e255e5e7437d9109540d157d21d849c090fc8a3
Thimma chowdary <thimma.chowdary@salientminds.com>
2023-11-30 05:40:47 +0000

    fixed bugs #8899

--- src/app/components/add-cr-version/add-cr-version.component.ts
+++ src/app/components/add-cr-version/add-cr-version.component.ts
@@ -1,4 +1,10 @@
-import { Component,Output,Input,EventEmitter,SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  SimpleChanges,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalStorageService } from 'src/app/components/services/local-storage.service';
 import { UtilsService } from 'src/app/components/services/utils.service';
@@ -8,19 +14,24 @@ import { CommentsService } from 'src/app/api/comments.service';
 @Component({
   selector: 'xnode-add-cr-version',
   templateUrl: './add-cr-version.component.html',
-  styleUrls: ['./add-cr-version.component.scss']
+  styleUrls: ['./add-cr-version.component.scss'],
 })
 export class AddCrVersionComponent {
   @Input() comment: any;
-  @Input()   latestVersion:any;
-  @Output() closeNewVersionPopUp = new EventEmitter<boolean>();
+  @Input() latestVersion: any;
+  @Output() closeNewVersionPopUp = new EventEmitter<object>();
   @Input() showVersionPopup: boolean = false;
   versionForm: FormGroup;
   submitted: boolean = false;
   product: any;
   currentUser: any;
 
-  constructor(private fb: FormBuilder,  private localStorageService: LocalStorageService,private utilsService:UtilsService,private commentsService: CommentsService,) {
+  constructor(
+    private fb: FormBuilder,
+    private localStorageService: LocalStorageService,
+    private utilsService: UtilsService,
+    private commentsService: CommentsService
+  ) {
     this.versionForm = this.fb.group({
       major: ['2311', [Validators.required, Validators.pattern(/^[.\d]+$/)]],
       minor: ['0', [Validators.required, Validators.pattern(/^[.\d]+$/)]],
@@ -33,20 +44,21 @@ export class AddCrVersionComponent {
 
   ngOnInit() {
     this.product = this.localStorageService.getItem(StorageKeys.Product);
-    this.currentUser = this.localStorageService.getItem(StorageKeys.CurrentUser);
-   
+    this.currentUser = this.localStorageService.getItem(
+      StorageKeys.CurrentUser
+    );
   }
 
-  ngOnChanges(changes: SimpleChanges){
-    if(this.latestVersion){
-      this.versionForm.patchValue({ build: this.latestVersion.build + 1 })
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.latestVersion) {
+      this.versionForm.patchValue({ build: this.latestVersion.build + 1 });
       this.versionForm.patchValue({ major: this.latestVersion.major });
       this.versionForm.patchValue({ minor: this.latestVersion.minor });
     }
   }
 
   closePopUp() {
-    this.closeNewVersionPopUp.emit(false);
+    this.closeNewVersionPopUp.emit({ refresh: false });
   }
 
   onMajorInputChange(event: Event) {
@@ -65,26 +77,41 @@ export class AddCrVersionComponent {
     }
     this.utilsService.loadSpinner(true);
     let body = {
-      "productId": this.product.id,
-      "major": this.versionForm.value.major,
-      "minor": this.versionForm.value.minor,
-      "build": this.versionForm.value.build,
-      "notes": {},
-      "attachments": [],
-      "createdBy": this.currentUser.user_id
-    }
-    this.commentsService.addVersion(body).then((response: any) => {
-      if (response.statusText === 'Created') {
-        this.utilsService.loadToaster({ severity: 'success', summary: 'SUCCESS', detail: 'Version added successfully' });
-        this.versionForm.reset();
-        this.closeNewVersionPopUp.emit(false);
-      } else {
-        this.utilsService.loadToaster({ severity: 'error', summary: 'ERROR', detail: response?.data?.common?.message });
-      }
-      this.utilsService.loadSpinner(false);
-    }).catch(err => {
-      this.utilsService.toggleTaskAssign(false);
-      this.utilsService.loadToaster({ severity: 'error', summary: 'ERROR', detail: err });
-    })
+      productId: this.product.id,
+      major: this.versionForm.value.major,
+      minor: this.versionForm.value.minor,
+      build: this.versionForm.value.build,
+      notes: {},
+      attachments: [],
+      createdBy: this.currentUser.user_id,
+    };
+    this.commentsService
+      .addVersion(body)
+      .then((response: any) => {
+        if (response.statusText === 'Created') {
+          this.utilsService.loadToaster({
+            severity: 'success',
+            summary: 'SUCCESS',
+            detail: 'Version added successfully',
+          });
+          this.versionForm.reset();
+          this.closeNewVersionPopUp.emit({ refresh: true });
+        } else {
+          this.utilsService.loadToaster({
+            severity: 'error',
+            summary: 'ERROR',
+            detail: response?.data?.common?.message,
+          });
+        }
+        this.utilsService.loadSpinner(false);
+      })
+      .catch((err) => {
+        this.utilsService.toggleTaskAssign(false);
+        this.utilsService.loadToaster({
+          severity: 'error',
+          summary: 'ERROR',
+          detail: err,
+        });
+      });
   }
 }
