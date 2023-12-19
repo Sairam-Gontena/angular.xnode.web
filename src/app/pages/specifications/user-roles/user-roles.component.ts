8c9e2b3ee5bd461bcede0e4ef765fd062c6beda0
arun.ragam <arun.ragam@salientminds.com>
2023-12-19 17:24:49 +0530

    user role changes

--- src/app/pages/specifications/user-roles/user-roles.component.ts
+++ src/app/pages/specifications/user-roles/user-roles.component.ts
@@ -27,6 +27,7 @@ export class UserRolesComponent implements OnInit {
   constructor(private specUtils: SpecUtilsService,private utilsService:UtilsService) { }
 
   ngOnInit(): void {
+    console.log(this.content)
   }
 
   getWords(subitem: any) {
