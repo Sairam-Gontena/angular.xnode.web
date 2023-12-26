620bf6ee448a2fe9c2682a656b4915a2cb573a65
arun.ragam <arun.ragam@salientminds.com>
2023-12-26 10:29:25 +0530

    9389 changes

--- src/app/pages/specifications/list-view/list-view.component.ts
+++ src/app/pages/specifications/list-view/list-view.component.ts
@@ -100,18 +100,38 @@ export class ListViewComponent {
   highlightSelectedText() {
     const selection = window.getSelection();
     this.selectedWordIndices = [];
+    // if (selection) {
+    //   if (!selection.rangeCount) return;
+    //   const range = selection.getRangeAt(0);
+    //   const elements = range.cloneContents().querySelectorAll('span');
+    //   elements.forEach(element => {
+    //     const id = element.id;
+    //     let index = id;
+    //     if (!this.selectedWordIndices.includes(index)) {
+    //       this.selectedWordIndices.push(index);
+    //     }
+    //   });
+    // }
     if (selection) {
       if (!selection.rangeCount) return;
       const range = selection.getRangeAt(0);
       const elements = range.cloneContents().querySelectorAll('span');
-      elements.forEach(element => {
-        const id = element.id;
-        let index = id;
-        if (!this.selectedWordIndices.includes(index)) {
-          this.selectedWordIndices.push(index);
-        }
-      });
-    }
+      if (elements.length === 0) {
+          const element = range.startContainer.parentElement;
+          if (element && element.id.startsWith('word')) {
+              const index = element.id; // change this line to get the correct index
+              this.selectedWordIndices.push(index);
+          }
+      } else {
+          elements.forEach(element => {
+              const id = element.id;
+              let index = id; // change this line to get the correct index
+              if (!this.selectedWordIndices.includes(index)) {
+                  this.selectedWordIndices.push(index);
+              }
+          });
+      }
+  }
   }
 
   onOverlayHide(){
