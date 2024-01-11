1ad00d41966a71a659849256c36d8d86ea17cbdb
arun.ragam <arun.ragam@salientminds.com>
2024-01-11 23:56:31 +0530

    quality assurance changes

--- src/app/pages/specifications/specifications.component.ts
+++ src/app/pages/specifications/specifications.component.ts
@@ -384,16 +384,11 @@ export class SpecificationsComponent implements OnInit, OnDestroy {
       }
       if (obj?.title == 'Quality Assurance') {
         let content = obj.content;
-        content.forEach((useCase: any) => {
-          useCase.TestCases=useCase.content;
-        });
-
         if (Array.isArray(obj.content)) {
           obj.content = [];
         }
-
         obj.content.push({
-          title: 'Test Cases',
+          title: 'Quality Assurance',
           content: content,
           id: obj.id,
         });
