ec0db5c519c8ae3c992f169435f855db3a5c2945
arun.ragam <arun.ragam@salientminds.com>
2024-01-11 17:10:23 +0530

    removed comments

--- src/app/pages/specifications/specifications.component.ts
+++ src/app/pages/specifications/specifications.component.ts
@@ -385,15 +385,7 @@ export class SpecificationsComponent implements OnInit, OnDestroy {
       if (obj?.title == 'Quality Assurance') {
         let content = obj.content;
         content.forEach((useCase: any) => {
-          useCase.TestCases=useCase.content
-          // if (useCase['Test Cases']) {
-          //   useCase.TestCases = useCase['Test Cases'];
-          //   delete useCase['Test Cases'];
-          //   useCase.TestCases.forEach((testCase: any) => {
-          //     testCase.TestCases = testCase['Test Cases'];
-          //     delete testCase['Test Cases'];
-          //   });
-          // }
+          useCase.TestCases=useCase.content;
         });
 
         if (Array.isArray(obj.content)) {
