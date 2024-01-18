d3d42db888318d887e22cbe960a95e2ab85a5011
arun.ragam <arun.ragam@salientminds.com>
2024-01-18 10:24:15 +0530

    9896, 10109 fix changes

--- src/app/components/product-dropdown/product-dropdown.component.ts
+++ src/app/components/product-dropdown/product-dropdown.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { UtilsService } from '../services/utils.service';
 import { Product } from 'src/models/product';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { SpecUtilsService } from '../services/spec-utils.service';
-import { delay, of } from 'rxjs';
 @Component({
   selector: 'xnode-product-dropdown',
   templateUrl: './product-dropdown.component.html',
@@ -19,26 +17,24 @@ export class ProductDropdownComponent implements OnInit {
   myForm: FormGroup;
 
   constructor(
-    private utilsService: UtilsService,
     private fb: FormBuilder,
     private specUtils: SpecUtilsService
   ) {
     this.myForm = this.fb.group({ selectedProduct: [null] });
     this.specUtils.getMeUpdatedProduct.subscribe((data: any) => {
       if (data) {
-        // this.getMeDataFromStorage();
         this.ngOnInit()
       }
     });
   }
 
   ngOnInit() {
-    this.product = localStorage.getItem('product');
-    this.getMeDataFromStorage();
     const metaData = localStorage.getItem('meta_data');
     if (metaData) {
       this.products = JSON.parse(metaData);
     }
+    this.product = localStorage.getItem('product');
+    this.getMeDataFromStorage();
     // this.myForm.valueChanges.subscribe((value: any) => {
     //   this.selectedProduct = value.selectedProduct;
     //   this._onChangeProduct.emit(this.selectedProduct);
@@ -53,20 +49,19 @@ export class ProductDropdownComponent implements OnInit {
   getMeDataFromStorage(): void {
     if (this.products) {
       if (this.product) {
-        of([]).pipe(delay(500)).subscribe((results) => {
+        setTimeout(() => {
           this.product = localStorage.getItem('product');
           this.selectedProduct = JSON.parse(this.product);
           this.myForm.patchValue({
             selectedProduct: this.products.find(
               (item: any) => item.id == this.selectedProduct.id
-            ),
-          });
-        });
+            ),});
+        }, 0);
       } else {
-        of([]).pipe(delay(500)).subscribe((results) => {
+        setTimeout(() => {
           this.selectedProduct = this.products[0];
           this.myForm.patchValue({ selectedProduct: this.selectedProduct });
-        });
+        }, 0);
       }
     }
   }
