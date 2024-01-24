cb23da56aef069d9ae3bd7be9bcf34742b86b17d
arun.ragam <arun.ragam@salientminds.com>
2024-01-24 21:08:51 +0530

    cr panel changes

--- src/app/pages/specifications/comments-panel/comments-panel.component.ts
+++ src/app/pages/specifications/comments-panel/comments-panel.component.ts
@@ -3,6 +3,8 @@ import { DropdownOptions } from 'src/models/dropdownOptions';
 import { SpecUtilsService } from 'src/app/components/services/spec-utils.service';
 import { LocalStorageService } from 'src/app/components/services/local-storage.service';
 import { StorageKeys } from 'src/models/storage-keys.enum';
+import { CommentsService } from 'src/app/api/comments.service';
+import { UtilsService } from 'src/app/components/services/utils.service';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 @Component({
@@ -11,25 +13,23 @@ import { debounceTime } from 'rxjs/operators';
   styleUrls: ['./comments-panel.component.scss'],
 })
 export class CommentsPanelComponent implements OnInit {
-  @Input() specData?: Array<[]>;
   @Input() usersList: any;
   @Input() list: any;
   @Input() activeIndex: any;
   @Input() swaggerData: any;
   showInput = false;
+  specData: any;
   filter: any;
   searchIconKeyword: string = '';
   selectedUsers: any = [];
+  status:any;
   filterOptions: Array<DropdownOptions> = [
     { label: 'All', value: 'ALL' },
     { label: 'Linked', value: 'LINKED' },
     { label: 'New', value: 'NEW' },
     { label: 'Closed', value: 'CLOSED' },
   ];
-  selectedFilter: { label: string; value: string } = {
-    label: 'All',
-    value: 'ALL',
-  };
+  selectedFilter: { label: string; value: string } = { label: 'All', value: 'ALL', };
   selectedComment: any;
   filteredList: any = [];
   product: any;
@@ -38,7 +38,9 @@ export class CommentsPanelComponent implements OnInit {
 
   constructor(
     private specUtils: SpecUtilsService,
-    private storageService: LocalStorageService
+    private storageService: LocalStorageService,
+    private apiComment:CommentsService,
+    private utils:UtilsService
   ) {
     this.product = this.storageService.getItem(StorageKeys.Product);
     this.searchUpdated.pipe(debounceTime(1000)).subscribe((search) => {
@@ -59,43 +61,64 @@ export class CommentsPanelComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.filterList();
+    this.specData = this.storageService.getItem(StorageKeys.SpecVersion);
+    console.log('commentspanelts',this.specData)
+    this.utils.loadSpinner(true);
+    this.getCommentsByStatus();
   }
 
   ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
     if (changes['list']?.currentValue) {
       this.filteredList = [];
       this.list = changes['list'].currentValue;
-      this.filterList();
+      // this.filterList();
     }
   }
 
-  filterList(data?: any): void {
-    this.filteredList = [];
-    switch (this.selectedFilter.value) {
-      case 'LINKED':
-        this.filteredList = this.list.filter(
-          (item: any) => item.status === 'LINKED'
-        );
-        break;
-      case 'UNLINKED':
-        this.filteredList = data.filter(
-          (item: any) => item.status === 'UNLINKED'
-        );
-        break;
-      case 'NEW':
-        this.filteredList = this.list.filter(
-          (item: any) => item.status === 'NEW'
-        );
-        break;
-      case 'CLOSED':
-        this.filteredList = this.list.filter(
-          (item: any) => item.status === 'CLOSED'
-        );
-        break;
-      default:
-        this.filteredList = this.list;
-        break;
+  getCommentsByStatus(status?:any){
+    console.log(status)
+    let query = 'comment/comments-by-productId?productId='+this.specData.productId+'&verisonId='+this.specData.id;
+    if(status){
+      query = 'comment/comments-by-productId?productId='+this.specData.productId+'&verisonId='+this.specData.id+'&status='+status;
     }
+    this.apiComment.getComments(query).then((res:any)=>{
+      if (res.status === 200 && res.data) {
+        this.list = res.data;
+        this.filteredList =  this.list;
+        // this.filterList(res.data);
+      }
+      this.utils.loadSpinner(false);
+    }).catch((err:any)=>{
+      this.utils.loadSpinner(false);
+    })
   }
+
+  // filterList(data?: any): void {
+  //   this.filteredList = [];
+  //   switch (this.selectedFilter.value) {
+  //     case 'LINKED':
+  //       this.filteredList = this.list.filter(
+  //         (item: any) => item.status === 'LINKED'
+  //       );
+  //       break;
+  //     case 'UNLINKED':
+  //       this.filteredList = data.filter(
+  //         (item: any) => item.status === 'UNLINKED'
+  //       );
+  //       break;
+  //     case 'NEW':
+  //       this.filteredList = this.list.filter(
+  //         (item: any) => item.status === 'NEW'
+  //       );
+  //       break;
+  //     case 'CLOSED':
+  //       this.filteredList = this.list.filter(
+  //         (item: any) => item.status === 'CLOSED'
+  //       );
+  //       break;
+  //     default:
+  //       this.filteredList = this.list;
+  //       break;
+  //   }
+  // }
 }
