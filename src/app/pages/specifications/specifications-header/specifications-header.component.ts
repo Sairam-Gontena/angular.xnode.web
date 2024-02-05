4c808d1724536f2c52a1cc6f26456a2e076b764a
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-02-05 11:50:17 +0530

    on spec header dropdown change chage cr tabs #10564

--- src/app/pages/specifications/specifications-header/specifications-header.component.ts
+++ src/app/pages/specifications/specifications-header/specifications-header.component.ts
@@ -243,6 +243,7 @@ export class SpecificationsHeaderComponent implements OnInit {
     this.showSpecGenaretePopup = false;
     this.utils.loadSpinner(true);
     let product = this.metaDeta.find((x: any) => x.id === obj.id);
+    this.specService.getMeCrList({ productId: product?.id });
     if (product && product.has_insights) {
       this.emitProductChange.emit(obj)
     } else {
