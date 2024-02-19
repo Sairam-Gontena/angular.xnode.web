d568a9eb6ab9766419db9795b274fe3500f7a263
anurag.t <anurag.t@salientminds.com>
2024-02-19 09:43:23 +0530

    feat: Designed the overall layout structure for the agent playground screen dashboard.

---
+++ src/app/pages/agent-hub/agent-hub.component.ts
@@ -0,0 +1,187 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'xnode-agent-hub',
+  templateUrl: './agent-hub.component.html',
+  styleUrls: ['./agent-hub.component.scss']
+})
+export class AgentHubComponent implements OnInit{
+  items:{ title: string}[] = [];
+  activeIndex = 0;
+
+  columns = [
+    {
+      field: "id",
+      filter: false,
+      header: "Id",
+      sortable: true,
+      width: 10
+    },
+    {
+      field: "agent",
+      filter: false,
+      header: "Agent Name",
+      sortable: true,
+      width: 30
+    },
+
+    {
+      field: "description",
+      filter: false,
+      header: "Description",
+      sortable: true,
+      width: 50
+    },
+    {
+      field: "state",
+      filter: false,
+      header: "State",
+      sortable: true,
+      width: 50
+    },
+    {
+      field: "status",
+      filter: false,
+      header: "Status",
+      sortable: true,
+      width: 50
+    },
+    {
+      field: "createdBy",
+      filter: false,
+      header: "Created By",
+      sortable: true,
+      width: 50
+    }
+  ];
+  
+
+  tableData = [
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+    {
+      id: "AGID002",
+      agent: "Navi v1.0",
+      description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+      state: "live",
+      status: "Active",
+      createdBy: "Anurag"
+    },
+  ]
+
+  tableInfo = {
+    delete_action: false,
+    export_action: false,
+    name: "Notification List",
+    search_input: true
+  }
+
+  dropdown = [
+    {
+        label: 'Update',
+        icon: 'pi pi-refresh',
+        command: () => {
+            // this.update();
+        }
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-times',
+        command: () => {
+            // this.delete();
+        }
+    },
+    // { label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io' },
+    // { separator: true },
+    // { label: 'Installation', icon: 'pi pi-cog', routerLink: ['/installation'] }
+];
+
+
+
+
+
+
+  selectedTab: number | null = 1;
+dropdownSelection: string = '';
+
+  dropdownItems = [
+    {label: 'Agents', value: 'Agent1'},
+    {label: 'Agents 2 6 8 0 8 9 7 6 5 ', value: 'Agent2'},
+    // more options...
+  ];
+  
+  selectTab(tabIndex: number) {
+    this.selectedTab = tabIndex;
+  }
+  
+  onDropdownChange(event:any) {
+    this.dropdownSelection = event.value;
+    this.selectedTab = null; // Or a specific value that doesn't match any tab index
+  }
+
+  test(s: string) {
+    console.log("hello",s)
+
+    this.activeIndex=0
+  }
+  ngOnInit() {
+    this.items =[
+      { title: 'Agents' },
+      { title: 'Capabilities' },
+      { title: 'Prompts' },
+
+      { title: 'Knowledge' },
+      { title: 'Models' },
+      { title: 'Tools' }
+  ];
+
+  }
+}
+
---
+++ src/app/pages/agent-hub/agent-hub.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AgentHubRoutingModule } from './agent-hub-routing.module';
+import { AgentHubComponent } from './agent-hub.component';
+import { SharedModule } from 'primeng/api';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { SharedComponentModule } from 'src/app/shared/shared-component.module';
+import { TabViewModule } from 'primeng/tabview';
+import { DropdownModule } from 'primeng/dropdown';
+import { SplitButtonModule } from 'primeng/splitbutton';
+
+@NgModule({
+  declarations: [
+    AgentHubComponent
+  ],
+  imports: [
+    CommonModule,
+    AgentHubRoutingModule,
+    TabMenuModule,
+
+    TabViewModule,
+    DropdownModule,
+    SplitButtonModule,
+    // SharedModule,
+    SharedComponentModule
+  ]
+})
+export class AgentHubModule { }
