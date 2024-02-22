f96e5ba99de3ca9fb9113987b281feb5d647a3fe
anurag.t <anurag.t@salientminds.com>
2024-02-22 23:15:59 +0530

    ref: created interface file for defining the types

--- src/app/pages/agent-hub/agent-hub.component.ts
+++ src/app/pages/agent-hub/agent-hub.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 import { AgentHubModel } from './agent-hub.model';
 
 @Component({
@@ -9,45 +8,10 @@ import { AgentHubModel } from './agent-hub.model';
 })
 export class AgentHubComponent implements OnInit {
 
-  agentHubModel: AgentHubModel;  
-  // dropdownSelection: string = '';
+  agentHubModel: AgentHubModel;
 
-  // dropdownItems = [
-  //   { label: 'Agents', value: 'Agent1' },
-  //   { label: 'Agents 2 6 8 0 8 9 7 6 5 ', value: 'Agent2' },
-  //   // more options...
-  // ];
-
-  // selectTab(tabIndex: number) {
-  //   this.selectedTab = tabIndex;
-  // }
-
-  // onDropdownChange(event: any) {
-  //   this.dropdownSelection = event.value;
-  //   // this.selectedTab = null; // Or a specific value that doesn't match any tab index
-  // }
-
-
-  ngOnInit() {}
-
-  // allColumns: any[] = [
-  //   { name: 'Australia', code: 'AU' },
-  //   { name: 'Brazil', code: 'BR' },
-  //   { name: 'China', code: 'CN' },
-  //   { name: 'Egypt', code: 'EG' },
-  //   { name: 'France', code: 'FR' },
-  //   { name: 'Germany', code: 'DE' },
-  //   { name: 'India', code: 'IN' },
-  //   { name: 'Japan', code: 'JP' },
-  //   { name: 'Spain', code: 'ES' },
-  //   { name: 'United States', code: 'US' }
-  // ];
-  // selectedColumns: any[] = [];
-
+  ngOnInit() { }
   constructor() {
-    // Set default columns to display
-    // this.selectedColumns = [...this.allColumns];
-
     this.agentHubModel = new AgentHubModel()
   }
 
--- src/app/pages/agent-hub/agent-hub.model.ts
+++ src/app/pages/agent-hub/agent-hub.model.ts
@@ -1,120 +1,125 @@
-import dynamicTableColumnData from "../../../assets/json/dynamictabledata.json"
+/**
+ * Represents the model for the Agent Hub page.
+ */
+import dynamicTableColumnData from "../../../assets/json/dynamictabledata.json";
+import { IDropdownItem, ITableDataEntry, ITableInfo } from "./IAgent-hub";
 
 export class AgentHubModel {
-    columns = dynamicTableColumnData.dynamicTable.AgentHub.columns;
-    activeIndex = 0;
-    items = [
-        { title: 'Agents' },
-        { title: 'Capabilities' },
-        { title: 'Prompts' },
-  
-        { title: 'Knowledge' },
-        { title: 'Models' },
-        { title: 'Tools' }
-      ];
-      tableData = [
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-        {
-          id: "AGID002",
-          agent: "Navi v1.0",
-          description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
-          state: "live",
-          status: "Active",
-          createdBy: "Anurag",
-          "action-btn": "..."
-        },
-      ]
+    columns: any; // Define the type of columns based on the actual data structure
+    activeIndex: number;
+    tabItems: { title: string }[];
+    tableData: ITableDataEntry[];
+    tableInfo: ITableInfo;
+    agentTabItemDropdown: IDropdownItem[];
 
-      tableInfo = {
-        delete_action: false,
-        export_action: false,
-        name: "Notification List",
-        search_input: true
-      }
-
-      agentTabItemDropdown = [
-        {
-          label: 'Update',
-          icon: 'pi pi-refresh',
-          command: () => {
-            // this.update();
-          }
-        },
-        {
-          label: 'Delete',
-          icon: 'pi pi-times',
-          command: () => {
-            // this.delete();
-          }
-        },
-        // { label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io' },
-        // { separator: true },
-        // { label: 'Installation', icon: 'pi pi-cog', routerLink: ['/installation'] }
-      ];
-    constructor () {}
+    constructor() {
+        this.columns = dynamicTableColumnData.dynamicTable.AgentHub.columns;
+        this.activeIndex = 0;
+        this.tabItems = [
+            { title: 'Agents' },
+            { title: 'Capabilities' },
+            { title: 'Prompts' },
+            { title: 'Knowledge' },
+            { title: 'Models' },
+            { title: 'Tools' }
+        ];
+        this.tableData = [
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            },
+            {
+                id: "AGID002",
+                agent: "Navi v1.0",
+                description: "Navi is a conversational assistant dedicated to lorem lupsum lorem lupsum",
+                state: "live",
+                status: "Active",
+                createdBy: "Anurag",
+                "action-btn": "..."
+            }
+        ];
+        this.tableInfo = {
+            delete_action: false,
+            export_action: false,
+            name: "Notification List",
+            search_input: true
+        };
+        this.agentTabItemDropdown = [
+            {
+                label: 'Update',
+                icon: 'pi pi-refresh',
+                command: () => {
+                    // this.update();
+                }
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-times',
+                command: () => {
+                    // this.delete();
+                }
+            },
+            // Additional dropdown items...
+        ];
+    }
 
 
 
     /**
-     * Methods define here
+     * Methods will be define from here
      */
-
-    test(s: string) {
-        console.log("hello", s)
-    
-        this.activeIndex = 0
-      }
+    test(s: string): void {
+        console.log("hello", s);
+        this.activeIndex = 0;
+    }
 }
