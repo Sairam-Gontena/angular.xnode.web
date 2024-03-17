c909219302dc27a57de4a8bca56d790920ffe16a
anurag.t <anurag.t@salientminds.com>
2024-03-18 00:44:34 +0530

    ref: added dynamic table and define meta property

--- src/app/pages/create-agent/create-agent.model.ts
+++ src/app/pages/create-agent/create-agent.model.ts
@@ -1,5 +1,13 @@
 import { LocalStorageService } from 'src/app/components/services/local-storage.service';
+import { IPaginatorInfo, ITableDataEntry, ITableInfo } from './ICreate-agent';
+import dynamicTableColumnData from '../../../assets/json/dynamictabledata.json';
 
+const InitialPaginatorInfo = {
+  page: 1,
+  perPage: 10,
+  totalPages: 0,
+  totalRecords: 0,
+};
 export class CreateAgentModel {
   tabItems: { idx: number; title: string; value: string }[] = [
     { idx: 0, title: 'Overview', value: 'overview' },
@@ -25,7 +33,98 @@ export class CreateAgentModel {
     { idx: 7, title: 'Tools', value: 'tool' },
   ];
 
+  tableData!: ITableDataEntry[];
+  columns: any; // Define the type of columns based on the actual data structure
+  tableInfo: ITableInfo = {
+    delete_action: false,
+    export_action: false,
+    name: 'Notification List',
+    search_input: true,
+  };
+
+  searchFilterOptions = {
+    showFilterOption: true,
+    filter: false,
+    showToggleAll: false,
+    showHeader: false,
+    options: [
+      {
+        idx: 0,
+        header: 'All',
+      },
+      {
+        idx: 1,
+        header: 'My Agents',
+      },
+    ],
+    placeholder: 'All',
+    optionLabel: 'header',
+    styleClass: 'custom-multiselect',
+  };
+
+  showColumnFilterOption = {
+    showFilterOption: true,
+    filter: false,
+    showToggleAll: false,
+    showHeader: false,
+    options: [],
+    placeholder: 'All',
+    optionLabel: 'header',
+    styleClass: 'showColumnFilterOption',
+    changeHandler: this.onShowDynamicColumnFilter.bind(this),
+  };
+
+  tableRowActionOptions = [
+    {
+      label: 'View',
+      icon: '',
+      command: (event: any) => {
+        console.log(event, 'event');
+      },
+    },
+    {
+      label: 'Duplicate',
+      icon: '',
+      command: (event: any) => {
+        console.log(event, 'event');
+      },
+    },
+    {
+      label: 'Archieve',
+      icon: '',
+      command: (event: any) => {
+        console.log(event, 'event');
+      },
+    },
+    {
+      label: 'Delete',
+      icon: '',
+      command: (event: any) => {
+        console.log(event, 'event');
+      },
+    },
+  ];
+
+  paginatorInfo: IPaginatorInfo = { ...InitialPaginatorInfo };
+
   activeIndex: number = 0;
 
   constructor(private storageService: LocalStorageService) {}
+
+  onShowDynamicColumnFilter(event: any) {
+    if (!event?.value?.length) {
+      this.columns =
+        dynamicTableColumnData?.dynamicTable?.AgentHub[
+          this.activeIndex
+        ]?.columns;
+    } else {
+      this.columns = dynamicTableColumnData?.dynamicTable?.AgentHub[
+        this.activeIndex
+      ].columns?.filter((item) =>
+        event?.value?.some(
+          (valItem: { idx: number }) => valItem.idx === item.idx
+        )
+      );
+    }
+  }
 }
