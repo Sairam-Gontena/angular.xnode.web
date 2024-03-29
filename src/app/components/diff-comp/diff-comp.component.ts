56f38d0213aac0aab7d2228692674269045e6d9b
Thimma Chowdary <thimma.chowdary@salientminds.com>
2024-03-29 05:11:06 -0700

    added missing token to dashboard iframe

--- src/app/components/diff-comp/diff-comp.component.ts
+++ src/app/components/diff-comp/diff-comp.component.ts
@@ -85,11 +85,12 @@ export class DiffCompComponent implements OnInit {
     this.product = this.storageService.getItem(StorageKeys.Product);
     this.currentUser = this.storageService.getItem(StorageKeys.CurrentUser);
     const version: any = this.storageService.getItem(StorageKeys.SpecVersion);
+    console.log(this.product,"this is product ")
     if (this.contentObj?.content_data_type === 'DASHBOARD' && this.currentUser) {
       this.targetUrl =
         environment.designStudioAppUrl +
         '?email=' +
-        this.currentUser?.email +
+        this.product?.createdBy?.email +
         '&id=' +
         this.product?.id +
         '&version_id=' +
