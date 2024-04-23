d5bed621c53eeabb2aff91eae4ef6f72c990a6c0
anurag.t <anurag.t@salientminds.com>
2024-04-23 20:02:58 +0530

    ref: Binded form value to the tools overview form

--- src/app/components/agent-hub/tools-details/tools-details.component.ts
+++ src/app/components/agent-hub/tools-details/tools-details.component.ts
@@ -1,32 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { agentName } from 'src/app/pages/agent-hub/agent-hub.constant';
-import { BreadCrumbsAction, CapabilitiesTableData, IPaginatorInfo, IQueryParams, ITableDataEntry, ITableInfo } from './ITools-details';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { BreadCrumbsAction } from './ITools-details';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { AgentHubService } from 'src/app/api/agent-hub.service';
-import { ActivatedRoute, Router } from '@angular/router';
 import { UtilsService } from '../../services/utils.service';
 import { StorageKeys } from 'src/models/storage-keys.enum';
-import { TabViewChangeEvent } from 'primeng/tabview';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'xnode-tools-details',
   templateUrl: './tools-details.component.html',
-  styleUrls: ['./tools-details.component.scss']
+  styleUrls: ['./tools-details.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ToolsDetailsComponent implements OnInit {
+export class ToolsDetailsComponent {
   @Input() rowViewData = {
     showDetail: false,
     showHeader: true,
     showTab: true,
     requestedId: ''
   }
-  agentInfo: any;
+  @Input() agentInfo: any;
   overviewForm!: FormGroup;
   tabItems: { idx: number; title: string; value: string, identifier: string }[] = [
     { idx: 0, title: 'Overview', value: 'overview', identifier: 'overview' },
-    { idx: 1, title: 'Actions', value: 'action', identifier: 'actions' },
+    // { idx: 1, title: 'Actions', value: 'action', identifier: 'actions' },
   ];
 
   activeIndex: number = 0;
@@ -43,19 +42,17 @@ export class ToolsDetailsComponent implements OnInit {
         },
       ],
     },
-    Action: {
-      buttonText: 'Action',
-      options: [
-        {
-          label: 'Add a Tool',
-          icon: '',
-          command: () => { },
-        },
-      ],
-    }
+    // Action: {
+    //   buttonText: 'Action',
+    //   options: [
+    //     {
+    //       label: 'Add a Tool',
+    //       icon: '',
+    //       command: () => { },
+    //     },
+    //   ],
+    // }
   };
-
-  activeHeaderActionBtnOption!: any[];
 
   breadCrumbsAction: BreadCrumbsAction = {
     isBreadCrumbActive: false,
@@ -69,78 +66,46 @@ export class ToolsDetailsComponent implements OnInit {
     // activeBreadCrumbsItem: "",
   };
 
-  currentActiveRowData: any;
-  overviewInstructionForm: any = {
-    enableOverview: false,
-    enableInstruction: false,
-    overviewInstructionData: ""
-  };
-
-
-
-  isFormEditable = false;
+  @Input() formEditable = false;
+  @Output() onEditSave = new EventEmitter<{ event: any }>();
 
   constructor(private storageService: LocalStorageService,
     private agentHubService: AgentHubService,
-    private activatedRoute: ActivatedRoute,
     private utilsService: UtilsService,
     private formBuilder: FormBuilder,
     private router: Router) {
     this.userInfo = this.storageService.getItem(StorageKeys.CurrentUser);
-  }
 
-  ngOnInit() {
+
     this.overviewForm = this.formBuilder.group({
       description: [''],
-      tags: ['']
+      name: [''],
+      actions: ['']
     })
-    this.getToolsDetailsById()
   }
-  pdateHeaderOption() {
-    let item = this.tabItems[this.activeIndex];
-    if (item.identifier in this.headerActionBtnOption) {
-      this.activeHeaderActionBtnOption =
-        this.headerActionBtnOption[
-          item.identifier as keyof typeof this.headerActionBtnOption
-        ].options;
-    } else {
-      console.error('Invalid identifier:', item.identifier);
-      // Handle the error appropriately
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes?.agentInfo?.currentValue) {
+      this.overviewForm.patchValue({
+        name: this.agentInfo.name,
+        description: this.agentInfo.description,
+        actions: this.agentInfo.action
+      });
+    }
+    if (changes.formEditable) {
+      if (this.formEditable) {
+        this.overviewForm.enable();
+      } else {
+        this.overviewForm.disable();
+      }
     }
   }
 
-  // tab event
-  onTabSwitchHandler(event: TabViewChangeEvent) {
-
-  }
-
-  //overview form submit
-  onOverviewSubmit() {
-    console.log(this.overviewForm.value);
-  }
-
-  //  get the agent details by Id
-  getToolsDetailsById() {
-    let url: string = "agent/tool_by_id/",
-      getID: any = this.rowViewData.requestedId,
-      urlParam: any = {
-        url: url + getID,
-        params: {}
-      }
-
-    this.agentHubService.getAgentDetail(urlParam).subscribe({
-      next: (response: any) => {
-        // this.getAgentDetailByCategorySuccess(response);
-        this.agentInfo = response
-        console.log(response, "response")
-      }, error: (error: any) => {
-        this.utilsService.loadToaster({ severity: 'error', summary: '', detail: error?.error?.detail });
-      }
-    });
-  }
-
-
   goBackBreadCrumbsHandler(event: any) {
+
+    // NOTE: Need to update this for tools. This is just dummy purpose.
+
     // this.breadCrumbsAction.activeBreadCrumbsItem = ""
     const newItem = this.breadCrumbsAction.breadcrumb;
     const indexToDelete = event.item.index + 1;
@@ -151,40 +116,10 @@ export class ToolsDetailsComponent implements OnInit {
       this.router.navigate([event.item.path]);
     }
 
-    // Show viewALl button
-    // this.viewAll.showButton = !this.breadCrumbsAction.isBreadCrumbActive;
-
     this.breadCrumbsAction.breadcrumb = [...newItem];
   }
+
   onEditSaveHandler() {
-    // const activeTab = this.tabItems[this.activeIndex].identifier
-
-    // if (this.isFormEditable) {
-    //   let urlParam = {
-    //     url: '',
-    //     data: {}
-    //   }
-
-    //   if (activeTab == agentName.prompt) {
-    //     const id = formData?.id
-
-    //     delete formData?.id
-    //     urlParam.url = `agent/update_prompt/${id}/${formData.version}`
-    //     urlParam.data = formData
-    //   }
-
-
-    //   this.agentHubService.updateData(urlParam).subscribe({
-    //     next: (response: any) => {
-    //       console.log("responseData", response)
-    //     }, error: (error: any) => {
-    //       console.log("responseData", error)
-    //     }
-    //   })
-    // }
-
-
-    // this.isFormEditable = !this.isFormEditable
-
+    this.onEditSave.emit(this.overviewForm?.value)
   }
 }
--- src/app/pages/tool-view/tool-view.component.ts
+++ src/app/pages/tool-view/tool-view.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AgentHubService } from 'src/app/api/agent-hub.service';
+import { UtilsService } from 'src/app/components/services/utils.service';
 
 @Component({
   selector: 'xnode-tool-view',
@@ -15,7 +17,10 @@ export class ToolViewComponent {
       requestedId: ''
     }
   }
-  constructor(private activatedRoute: ActivatedRoute) { }
+  agentInfo: any;
+
+  isFormEditable = false;
+  constructor(private activatedRoute: ActivatedRoute, private agentHubService: AgentHubService, private utilsService: UtilsService,) { }
 
 
   ngOnInit(): void {
@@ -23,8 +28,39 @@ export class ToolViewComponent {
     if (toolId) {
       this.rowViewData.tool.requestedId = toolId
       this.rowViewData.tool.showHeader = true
+
+      this.getToolsDetailsById()
     } else {
       // Handel the case and show error 
+      this.utilsService.loadToaster({ severity: 'error', summary: '', detail: 'Unable to fetch tool Id' });
     }
+  }
+
+  //  get the agent details by Id
+  getToolsDetailsById() {
+    let url: string = "agent/tool_by_id/",
+      getID: any = this.rowViewData.tool.requestedId,
+      urlParam: any = {
+        url: url + getID,
+        params: {}
+      }
+
+    this.agentHubService.getAgentDetail(urlParam).subscribe({
+      next: (response: any) => {
+        // this.getAgentDetailByCategorySuccess(response);
+        this.agentInfo = response
+        console.log(response, "response")
+      }, error: (error: any) => {
+        this.utilsService.loadToaster({ severity: 'error', summary: '', detail: error?.error?.detail });
+      }
+    });
+  }
+
+  onEditSaveHandler(formData: any) {
+
+    if (this.isFormEditable) {
+      // Handle Save Case.
+    }
+    this.isFormEditable = !this.isFormEditable
   }
 }
