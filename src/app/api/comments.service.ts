6d4bcda1605d74ef19740dcf4c7cedf8cc87cf4a
arun.ragam <arun.ragam@salientminds.com>
2024-05-15 17:36:39 +0530

    type error on see more of comments section fix

--- src/app/api/comments.service.ts
+++ src/app/api/comments.service.ts
@@ -33,7 +33,7 @@ export class CommentsService extends BaseApiService {
       '/task/tasks-by-productId?productId=' +
       params.productId +
       '&verisonId=' +
-      params.versionId + '&replyCountRequired=true';
+      params.versionId;
     if (params.status) {
       url = url + '&status=' + params.status;
     }
@@ -43,6 +43,11 @@ export class CommentsService extends BaseApiService {
   getTasks(params?: any) {
     let url = '/task';
     return this.get(url, params);
+  }
+
+  getTasksByOnlyProductId(params:any){
+    let url = '/task/tasks-by-only-productId?productId=' +  params.productId + '&replyCountRequired=true';
+    return this.get(url);
   }
 
   updateComments(body: any) {
