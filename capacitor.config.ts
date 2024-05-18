b7c31d19d1eac4bc5439f634facba6a6d41a8360
Krishna <krishna.c@salientminds.com>
2024-05-18 11:48:24 -0400

    android build changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,39 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
+let config: CapacitorConfig;
+
+
+const baseConfig: CapacitorConfig = {
   appId: 'ai.xnode.xnode',
   appName: 'xnode',
-  webDir: 'dist/xnode'
+  webDir: 'dist/xnode',
+  bundledWebRuntime: false
 };
+
+switch (process.env.NODE_ENV) {
+  case 'prod':
+    config = {
+      ...baseConfig,
+      ios: {
+        scheme: 'App',
+      },
+      android: {
+        flavor: 'dev',
+      },
+    };
+    break;
+  default:
+    config = {
+      ...baseConfig,
+      ios: {
+        scheme: 'App QA',
+      },
+      android: {
+        flavor: 'qa',
+      },
+    };
+    break;
+}
+
 
 export default config;
