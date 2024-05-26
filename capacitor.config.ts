a63fa203c35b30c8fff79d44d0a9b22145f6407e
Krishna <krishna.c@salientminds.com>
2024-05-26 08:25:57 -0400

    changes to mobile app configuration with trapeze

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,39 +1,51 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { environment } from './src/environments/environment';
+import { MobileProject, MobileProjectConfig } from '@trapezedev/project';
 
-let config: CapacitorConfig;
-
-
-const baseConfig: CapacitorConfig = {
-  appId: 'ai.xnode.xnode',
-  appName: 'xnode',
+const config: CapacitorConfig = {
+  appId: environment.mobileApp.appId,
+  appName: environment.mobileApp.name,
   webDir: 'dist/xnode',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
-switch (process.env.NODE_ENV) {
-  case 'prod':
-    config = {
-      ...baseConfig,
-      ios: {
-        scheme: 'App',
-      },
-      android: {
-        flavor: 'dev',
-      },
-    };
-    break;
-  default:
-    config = {
-      ...baseConfig,
-      ios: {
-        scheme: 'App QA',
-      },
-      android: {
-        flavor: 'qa',
-      },
-    };
-    break;
+const projectConfig: MobileProjectConfig = {
+  ios: {
+    path: 'ios'
+  },
+  android: {
+    path: 'android'
+  }
+};
+const updateProject = async () => {
+  const project = new MobileProject('./', projectConfig);
+  await project.load();
+  try {
+
+    await project.android?.setAppName(environment.mobileApp.name);
+    await project.android?.setPackageName(environment.mobileApp.appId); 
+    
+    const gradleFile = project.android?.getAppBuildGradle();
+    await gradleFile?.setApplicationId(environment.mobileApp.appId);
+    await gradleFile?.setNamespace(environment.mobileApp.appId);
+
+    const buildGradle = project.android?.getBuildGradle();
+    await buildGradle?.setApplicationId(environment.mobileApp.appId);
+    await buildGradle?.setNamespace(environment.mobileApp.appId);
+
+   
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    const appTarget = project.ios?.getAppTarget();
+    project.ios?.setBundleId(appTarget?.name ?? "App", null, environment.mobileApp.appId);
+    await project.ios?.setDisplayName(appTarget?.name ?? "App", null, environment.mobileApp.name);
+  } catch (error) {
+    console.log(error);
+  }
+  await project.commit();
 }
-
-
+updateProject().then((result)=>{console.log('updated project')});
 export default config;
